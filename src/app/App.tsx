88f8bdaf79bea6e6d1cd87ca45e58c22d64df287
z-e-a <e.a.zubkov@yandex.ru>
2024-07-17 19:20:54 +0300

    feat: add pagination

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,7 @@ import { useCallback, useEffect, useState } from "react";
 import { IPeople } from "../SWApi";
 import Header from "../widgets/Header";
 import PeopleList from "../widgets/PeopleList";
+import Paginator from "../widgets/Paginator";
 import { useLocalStorage } from "./useLocalStorage";
 import { Outlet, useSearchParams } from "react-router-dom";
 
@@ -18,20 +19,44 @@ function App() {
   }
 
   const [people, setPeople] = useState<IPeople[]>([]);
+  const [totalItemsCount, setTotalItemsCount] = useState<number>(-1);
+
+  const currentPage = searchParams.get("page") ?? "1";
 
   const search = useCallback(
     (text: string) => {
       const trimmedText = text.trim();
       setSearchText(trimmedText);
+
+      const redusedSearchParams: URLSearchParams = new URLSearchParams();
+      searchParams.forEach((v, k) => {
+        if (k !== "text" && k !== "page") {
+          redusedSearchParams.set(k, v);
+        }
+      });
+
       if (trimmedText) {
-        setSearchParams({ text: trimmedText });
-      } else {
-        setSearchParams({});
+        redusedSearchParams.set("text", trimmedText);
       }
-      fetch(`${import.meta.env.VITE_API_URL}?search=${trimmedText}`)
+      if (currentPage !== "1") {
+        redusedSearchParams.set("page", currentPage);
+      }
+
+      setSearchParams(redusedSearchParams);
+
+      const url = new URL(import.meta.env.VITE_API_URL);
+      if (trimmedText) {
+        url.searchParams.set("search", trimmedText);
+      }
+      console.log(currentPage);
+      if (currentPage !== "1") {
+        url.searchParams.set("page", String(currentPage));
+      }
+      fetch(url.toString())
         .then((response) => response.json())
         .then((data) => {
           setPeople(data.results);
+          setTotalItemsCount(data.count);
         })
         .catch((error) => console.log(error));
     },
@@ -40,11 +65,16 @@ function App() {
 
   useEffect(() => {
     search(searchText);
-  }, []);
+  }, [currentPage]);
 
   return (
     <>
       <Header searchText={searchText} searchCallback={search}></Header>
+      <Paginator
+        currentPage={parseInt(currentPage, 1)}
+        pageSize={import.meta.env.VITE_PAGITAOR_PAGE_SIZE}
+        totalItemsCount={totalItemsCount}
+      />
       <PeopleList people={people}>
         <Outlet />
       </PeopleList>
