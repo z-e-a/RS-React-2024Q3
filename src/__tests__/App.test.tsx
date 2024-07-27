99825454f03c6f7143f7ea1cafb0282dac52e1b7
z-e-a <e.a.zubkov@yandex.ru>
2024-07-27 12:44:48 +0300

    feat: update test for App component

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,10 +1,35 @@
-import { test } from "vitest";
+import { expect, test } from "vitest";
 import { createRoot } from "react-dom/client";
 import App from "../app/App";
+import { renderWithProviders } from "./test-utils";
+import { ThemeContext } from "../app/Contexts";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
 
-test("renders without crashing", () => {
+const preloadedState = {
+  peopleView: {
+    currentPage: 1,
+    totalItemsCount: 1,
+    selectedPeople: [],
+    searchText: "test",
+  },
+};
+
+test("Render App component without crashing", () => {
   const div = document.createElement("div");
   const root = createRoot(div);
-  root.render(<App />);
+  const component = renderWithProviders(
+    <ThemeContext.Provider value={"dark"}>
+      <MemoryRouter
+        initialEntries={["/search/detail?name=Luke+Skywalker&id=1&text=lu"]}
+      >
+        <Routes>
+          <Route path="*" element={<App />} />
+        </Routes>
+      </MemoryRouter>
+    </ThemeContext.Provider>,
+    { preloadedState },
+  );
+
+  expect(component).toMatchSnapshot();
   root.unmount();
 });
